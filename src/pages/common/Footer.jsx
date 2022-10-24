df910351639324a06c38b3f19a3ff342b1bc3184
ajw1079 <ajw1079@naver.com>
2022-10-24 21:29:17 +0900

    merge

---
+++ src/pages/common/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>Copyrights &copy; AdAge Ltd.</p>
+            </footer>
+        </>
+    )
+};
+export default Footer;
